971aa509168023779004ea707a2d0b31705bf6c4
Tarik Dadi <daditarik@gmail.com>
2014-02-16 15:17:23 +0100

    rationalisation des requêtes http

--- atp.py
+++ atp.py
@@ -17,31 +17,28 @@ URL = "http://www.atpworldtour.com/Share/Event-Draws.aspx?e=540&y=2013"
 # Fonction qui renvoie le html parsé
 def make_soup(url):
     html = urlopen(url).read()
-    return BeautifulSoup(html, "html.parser")
+    soup = BeautifulSoup(html, "html.parser")
+    return soup
 
 # Fonction qui récupère le nom du tournoi
-def get_tournament_title(section_url):
-    soup = make_soup(section_url)
+def get_tournament_title(soup):
     title = soup.find("a", "tournamentTitle").string
     return title
 
 # Fonction qui récupère le lieu du tournoi
-def get_tournament_location(section_url):
-    soup = make_soup(section_url)
+def get_tournament_location(soup):
     location = soup.find("p", "tournamentSubTitle").string.split(" -")[0]
     return location
 
 # Fonction qui récupère la date de début et de fin du tournoi
-def get_tournament_date(section_url):
-    soup = make_soup(section_url)
+def get_tournament_date(soup):
     date = soup.find("p", "tournamentSubTitle").string.split("- ")[1]
     debut = date.split("-")[0].encode('utf-8')
     fin = date.split("-")[1].encode('utf-8')
     return debut, fin
 
 # Fonction qui récupère le type de surface
-def get_tournament_surface(section_url):
-    soup = make_soup(section_url)
+def get_tournament_surface(soup):
     inlineWrapper = soup.find("span", "inlineWrapper")
     surface = inlineWrapper.findAll("p")[1]
     surface = str(surface).split(">")[3]
@@ -49,8 +46,7 @@ def get_tournament_surface(section_url):
     return surface
 
 # Fonction qui récupère la dotation du tournoi
-def get_tournament_prizemoney(section_url):
-    soup = make_soup(section_url)
+def get_tournament_prizemoney(soup):
     inlineWrapper = soup.find("span", "inlineWrapper")
     prizemoney = inlineWrapper.findAll("p")[2]
     prizemoney = str(prizemoney).split(">")[3]
@@ -58,8 +54,7 @@ def get_tournament_prizemoney(section_url):
     return prizemoney
 
 # Fonction qui récupère le nom et prénom de tous les joueurs du premier tour
-def get_player_name_first_round(section_url):
-    soup = make_soup(section_url)
+def get_player_name_first_round(soup):
     colonne1 = soup.find("td", "col_1")
     playerWrap = colonne1.findAll("div", "playerWrap")
     list_player_first_round = []
@@ -72,16 +67,14 @@ def get_player_name_first_round(section_url):
 ######################################################################################################
 
 # Fonction qui récupère le nom et prénom de tous les gagnants du premier tour
-def get_player_name_first_round_winner(section_url):
-    soup = make_soup(section_url)
+def get_player_name_first_round_winner(soup):
     colonne2 = soup.find("td", "col_2")
     playerWrap = colonne2.findAll("div", "playerWrap")
     for name in playerWrap:
     	print name.find("a").string
 
 # Fonction qui récupère le score du match du premier tour
-def get_player_score_first_round_winner(section_url):
-    soup = make_soup(section_url)
+def get_player_score_first_round_winner(soup):
     colonne2 = soup.find("td", "col_2")
     scores = colonne2.findAll("div", "scores")
     for score in scores:
@@ -90,16 +83,14 @@ def get_player_score_first_round_winner(section_url):
 ######################################################################################################
 
 # Fonction qui récupère le nom et prénom de tous les gagnants du deuxième tour
-def get_player_name_second_round_winner(section_url):
-    soup = make_soup(section_url)
+def get_player_name_second_round_winner(soup):
     colonne3 = soup.find("td", "col_3")
     playerWrap = colonne3.findAll("div", "playerWrap")
     for name in playerWrap:
     	print name.find("a").string
 
 # Fonction qui récupère le score du match du deuxième tour
-def get_player_score_second_round_winner(section_url):
-    soup = make_soup(section_url)
+def get_player_score_second_round_winner(soup):
     colonne3 = soup.find("td", "col_3")
     scores = colonne3.findAll("div", "scores")
     for score in scores:
@@ -109,16 +100,14 @@ def get_player_score_second_round_winner(section_url):
 ######################################################################################################
 
 # Fonction qui récupère le nom et prénom de tous les gagnants du troisième tour
-def get_player_name_third_round_winner(section_url):
-    soup = make_soup(section_url)
+def get_player_name_third_round_winner(soup):
     colonne4 = soup.find("td", "col_4")
     playerWrap = colonne4.findAll("div", "playerWrap")
     for name in playerWrap:
     	print name.find("a").string
 
 # Fonction qui récupère le score du match du troisième tour
-def get_player_score_third_round_winner(section_url):
-    soup = make_soup(section_url)
+def get_player_score_third_round_winner(soup):
     colonne4 = soup.find("td", "col_4")
     scores = colonne4.findAll("div", "scores")
     for score in scores:
@@ -126,15 +115,13 @@ def get_player_score_third_round_winner(section_url):
 
 
 
-def get_player_name_fourth_round_winner(section_url):
-    soup = make_soup(section_url)
+def get_player_name_fourth_round_winner(soup):
     colonne5 = soup.find("td", "col_5")
     playerWrap = colonne5.findAll("div", "playerWrap")
     for name in playerWrap:
     	print name.find("a").string
 
-def get_player_score_fourth_round_winner(section_url):
-    soup = make_soup(section_url)
+def get_player_score_fourth_round_winner(soup):
     colonne5 = soup.find("td", "col_5")
     scores = colonne5.findAll("div", "scores")
     for score in scores:
@@ -142,15 +129,13 @@ def get_player_score_fourth_round_winner(section_url):
 
 
 
-def get_player_name_fifth_round_winner(section_url):
-    soup = make_soup(section_url)
+def get_player_name_fifth_round_winner(soup):
     colonne6 = soup.find("td", "col_6")
     playerWrap = colonne6.findAll("div", "playerWrap")
     for name in playerWrap:
     	print name.find("a").string
 
-def get_player_score_fifth_round_winner(section_url):
-    soup = make_soup(section_url)
+def get_player_score_fifth_round_winner(soup):
     colonne6 = soup.find("td", "col_6")
     scores = colonne6.findAll("div", "scores")
     for score in scores:
@@ -158,30 +143,26 @@ def get_player_score_fifth_round_winner(section_url):
 
 
 
-def get_player_name_sixth_round_winner(section_url):
-    soup = make_soup(section_url)
+def get_player_name_sixth_round_winner(soup):
     colonne7 = soup.find("td", "col_7")
     playerWrap = colonne7.findAll("div", "playerWrap")
     for name in playerWrap:
     	print name.find("a").string
 
-def get_player_score_sixth_round_winner(section_url):
-    soup = make_soup(section_url)
+def get_player_score_sixth_round_winner(soup):
     colonne7 = soup.find("td", "col_7")
     scores = colonne7.findAll("div", "scores")
     for score in scores:
     	print score.find("a").string
 
 
-def get_player_name_seventh_round_winner(section_url):
-    soup = make_soup(section_url)
+def get_player_name_seventh_round_winner(soup):
     colonne8 = soup.find("td", "col_8")
     playerWrap = colonne8.findAll("div", "playerWrap")
     for name in playerWrap:
     	return name.find("a").string
 
-def get_player_score_seventh_round_winner(section_url):
-    soup = make_soup(section_url)
+def get_player_score_seventh_round_winner(soup):
     colonne8 = soup.find("td", "col_8")
     scores = colonne8.findAll("div", "scores")
     for score in scores:
@@ -229,23 +210,26 @@ tournamentYear_urls = [BASE_URL_YEAR + str(year) for year in range(1996,2014)]
 
 tournamentName_urls = [BASE_URL_NAME + str(rg) + "&y=2012"]
 
-mon_fichier.write(get_tournament_title(URL)+"\n")
-mon_fichier.write(get_tournament_location(URL)+"\n")
-mon_fichier.write(str(get_tournament_date(URL))+"\n")
-mon_fichier.write(str(get_tournament_surface(URL))+"\n")
-mon_fichier.write(str(get_tournament_prizemoney(URL))+"\n")
+soup = make_soup(URL)
+
+mon_fichier.write(get_tournament_title(soup)+"\n")
+mon_fichier.write(get_tournament_location(soup)+"\n")
+mon_fichier.write(str(get_tournament_date(soup))+"\n")
+mon_fichier.write(str(get_tournament_surface(soup))+"\n")
+mon_fichier.write(str(get_tournament_prizemoney(soup))+"\n")
 mon_fichier.write(""+"\n")
 
-mon_fichier.write("Gagnants de " + str(get_tournament_title(URL)) + " de 1996 à 2013"+"\n\n")
+mon_fichier.write("Gagnants de " + str(get_tournament_title(soup)) + " de 1996 à 2013"+"\n\n")
 mon_fichier.write("Année		Joueurs 				Scores"+"\n\n")
 
 tournamentYear = 1996
 
 for tournamentYear_url in tournamentYear_urls:
-	mon_fichier.write(str(tournamentYear) + "		")
-	mon_fichier.write(get_player_name_seventh_round_winner(tournamentYear_url)+"			")
-	mon_fichier.write(get_player_score_seventh_round_winner(tournamentYear_url)+"\n")
-	tournamentYear+=1
+    soup = make_soup(tournamentYear_url)
+    mon_fichier.write(str(tournamentYear) + "		")
+    mon_fichier.write(get_player_name_seventh_round_winner(soup)+"			")
+    mon_fichier.write(get_player_score_seventh_round_winner(soup)+"\n")
+    tournamentYear+=1
 
 
 mon_fichier.close
--- atp_first_round.py
+++ atp_first_round.py
@@ -9,39 +9,32 @@ sys.setrecursionlimit(30000)
 
 mon_fichier = open("fichier.txt", "w")
 
-# URL du drawing pour un tournoi une année précise
-
-URL = "http://www.atpworldtour.com/Share/Event-Draws.aspx?e=540&y=2013"
-
 
 # Fonction qui renvoie le html parsé
 def make_soup(url):
     html = urlopen(url).read()
-    return BeautifulSoup(html, "html.parser")
+    soup = BeautifulSoup(html, "html.parser")
+    return soup
 
 # Fonction qui récupère le nom du tournoi
-def get_tournament_title(section_url):
-    soup = make_soup(section_url)
+def get_tournament_title(soup):
     title = soup.find("a", "tournamentTitle").string
     return title
 
 # Fonction qui récupère le lieu du tournoi
-def get_tournament_location(section_url):
-    soup = make_soup(section_url)
+def get_tournament_location(soup):
     location = soup.find("p", "tournamentSubTitle").string.split(" -")[0]
     return location
 
 # Fonction qui récupère la date de début et de fin du tournoi
-def get_tournament_date(section_url):
-    soup = make_soup(section_url)
+def get_tournament_date(soup):
     date = soup.find("p", "tournamentSubTitle").string.split("- ")[1]
     debut = date.split("-")[0].encode('utf-8')
     fin = date.split("-")[1].encode('utf-8')
     return debut, fin
 
 # Fonction qui récupère le type de surface
-def get_tournament_surface(section_url):
-    soup = make_soup(section_url)
+def get_tournament_surface(soup):
     inlineWrapper = soup.find("span", "inlineWrapper")
     surface = inlineWrapper.findAll("p")[1]
     surface = str(surface).split(">")[3]
@@ -49,8 +42,7 @@ def get_tournament_surface(section_url):
     return surface
 
 # Fonction qui récupère la dotation du tournoi
-def get_tournament_prizemoney(section_url):
-    soup = make_soup(section_url)
+def get_tournament_prizemoney(soup):
     inlineWrapper = soup.find("span", "inlineWrapper")
     prizemoney = inlineWrapper.findAll("p")[2]
     prizemoney = str(prizemoney).split(">")[3]
@@ -58,8 +50,7 @@ def get_tournament_prizemoney(section_url):
     return prizemoney
 
 # Fonction qui récupère le nom et prénom de tous les joueurs du premier tour
-def get_player_name_first_round(section_url):
-    soup = make_soup(section_url)
+def get_player_name_first_round(soup):
     colonne1 = soup.find("td", "col_1")
     playerWrap = colonne1.findAll("div", "playerWrap")
     list_player_first_round = []
@@ -71,21 +62,26 @@ def get_player_name_first_round(section_url):
 ######################################################################################################
 
 # Fonction qui récupère le nom et prénom de tous les gagnants du premier tour
-def get_player_name_first_round_winner(section_url):
-    soup = make_soup(section_url)
+def get_player_name_first_round_winner(soup):
     colonne2 = soup.find("td", "col_2")
     playerWrap = colonne2.findAll("div", "playerWrap")
     for name in playerWrap:
     	print name.find("a").string
 
 # Fonction qui récupère le score du match du premier tour
-def get_player_score_first_round_winner(section_url):
-    soup = make_soup(section_url)
+def get_player_score_first_round_winner(soup):
     colonne2 = soup.find("td", "col_2")
     scores = colonne2.findAll("div", "scores")
     for score in scores:
     	return score.a.string
 
+##############################################################################################
+
+# URL du drawing pour un tournoi une année précise
+
+URL = "http://www.atpworldtour.com/Share/Event-Draws.aspx?e=540&y=2013"
+
+##############################################################################################
 
 BASE_URL_YEAR = "http://www.atpworldtour.com/Share/Event-Draws.aspx?e=540&y="
 
@@ -97,47 +93,39 @@ tournamentYear_urls = [BASE_URL_YEAR + str(year) for year in range(1996,2014)]
 
 tournamentName_urls = [BASE_URL_NAME + str(rg) + "&y=2012"]
 
-mon_fichier.write(get_tournament_title(URL)+"\n")
-mon_fichier.write(get_tournament_location(URL)+"\n")
-mon_fichier.write(str(get_tournament_date(URL))+"\n")
-mon_fichier.write(str(get_tournament_surface(URL))+"\n")
-mon_fichier.write(str(get_tournament_prizemoney(URL))+"\n")
+soup = make_soup(URL)
+
+mon_fichier.write(get_tournament_title(soup)+"\n")
+mon_fichier.write(get_tournament_location(soup)+"\n")
+mon_fichier.write(str(get_tournament_surface(soup))+"\n")
+mon_fichier.write(str(get_tournament_prizemoney(soup))+"\n")
 mon_fichier.write(""+"\n")
 
-
+mon_fichier.write("ANNEE" + "        ")
 mon_fichier.write("TOUR" + "                  ")
-mon_fichier.write("JOUEUR 1" + "       ")
-mon_fichier.write("JOUEUR 2" + "       ")
+mon_fichier.write("JOUEUR 1" + "             ")
+mon_fichier.write("JOUEUR 2" + "              ")
 mon_fichier.write("SCORE"+"\n\n")
 
-'''
+tournamentYear = 1996
+
 for tournamentYear_url in tournamentYear_urls:
-    mon_fichier.write(str(tournamentYear) + "       ")
-    mon_fichier.write(str(get_tournament_surface(URL))+"        ")
-    mon_fichier.write(str(get_tournament_prizemoney(URL))+"         ")
-    mon_fichier.write(str("Grand Chelem         ")
-    mon_fichier.write(get_tournament_title(URL)+"       ")
-    mon_fichier.write(str("Fisrt         ")
-    mon_fichier.write(get_player_name_first_round(tournamentYear_url))
-    mon_fichier.write(get_player_name_seventh_round_winner(tournamentYear_url)+"            ")
-    mon_fichier.write(get_player_score_seventh_round_winner(tournamentYear_url)+"\n")
+    i = 0
     tournamentYear+=1
-'''
-
-
-i = 0
-#for i in len(get_player_name_first_round(URL))/2:
-while i < len(get_player_name_first_round(URL)):
-    #mon_fichier.write("2013" + "       ")
-    #mon_fichier.write(str(get_tournament_surface(URL))+"        ")
-    #mon_fichier.write(str(get_tournament_prizemoney(URL))+"         ")
-    #mon_fichier.write(str("Grand Chelem         "))
-    #mon_fichier.write(get_tournament_title(URL)+"       ")
-    mon_fichier.write(str("Fisrt Round         "))
-    mon_fichier.write(get_player_name_first_round(URL)[i] + "       ")
-    mon_fichier.write(get_player_name_first_round(URL)[i+1] + "        ")
-    mon_fichier.write(get_player_score_first_round_winner(URL) + "\n")
-    i+=2
+    soup = make_soup(tournamentYear_url)
+    #for i in len(get_player_name_first_round(URL))/2:
+    while i < len(get_player_name_first_round(soup)):
+        #mon_fichier.write("2013" + "       ")
+        #mon_fichier.write(str(get_tournament_surface(URL))+"        ")
+        #mon_fichier.write(str(get_tournament_prizemoney(URL))+"         ")
+        #mon_fichier.write(str("Grand Chelem         "))
+        #mon_fichier.write(get_tournament_title(URL)+"       ")
+        mon_fichier.write(str(tournamentYear) + "       ")
+        mon_fichier.write(str("Fisrt Round         "))
+        mon_fichier.write(get_player_name_first_round(soup)[i] + "       ")
+        mon_fichier.write(get_player_name_first_round(soup)[i+1] + "        ")
+        mon_fichier.write(get_player_score_first_round_winner(soup) + "\n")
+        i+=2
 
 
 
